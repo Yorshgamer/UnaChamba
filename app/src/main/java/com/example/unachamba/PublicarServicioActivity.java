72117568a98254ab1b86d382cfdfb5c7aeb91a48
Gerald1253 <73809994@continental.edu.pe>
2024-06-23 01:12:46 -0500

    correcion de manifest

--- app/src/main/java/com/example/unachamba/PublicarServicioActivity.java
+++ app/src/main/java/com/example/unachamba/PublicarServicioActivity.java
@@ -8,17 +8,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ServiceActivity extends AppCompatActivity {
+public class PublicarServicioActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_service);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.activity_publicarservicio);
     }
 }
